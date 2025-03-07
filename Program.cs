fa583848646c45969c51577ccfc93a2fa5e8c239
jes <jesterresma@gmail.com>
2025-03-07 13:36:51 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 
 namespace FileCreation
 {
@@ -7,62 +8,107 @@ namespace FileCreation
     {
         static void Main(string[] args)
         {
-            string[] options = { "[1] Create", "[2] Open", "[3] Rename", "[4] Delete" };
+            string[] options = { "[1] Create", "[2] Open", "[3] Rename", "[4] Edit", "[5] Delete"};
             int opt, createOpt;
+            bool invalidOption, clear;
 
             List<string> list = new List<string>();
 
-            Console.WriteLine("FILE CREATION");
+            do
+            {
+                Console.WriteLine("FILE CREATION");
+                clear = false;
 
-            foreach(string i in options) {
-                Console.WriteLine(i);
-            }
-
-            do {
-                Console.Write("\nSelect an option: ");
-                opt = Convert.ToInt16(Console.ReadLine());
-
-                switch (opt)
+                foreach (string i in options)
                 {
-                    case 1:
-                        Console.WriteLine("\nCreate a Folder/Text File:");
-                        Console.WriteLine("[1] Folder \n[2] Text File");
-                        
-                        Console.Write("\nSelect an option: ");
-                        createOpt = Convert.ToInt16(Console.ReadLine());
-
-                        if(createOpt==1)
-                        {
-                            Console.WriteLine("\nEnter a name: ");
-                            string folderName = Console.ReadLine();
-                            list.Add(folderName);
-
-                            Console.WriteLine("Folder created succesfully!");
-                        }
-                        break;
-                    case 2:
-                        Console.WriteLine("\nOpen a Folder/Text File:");
-                        //Files shown below
-                        break;
-                    case 3:
-                        Console.WriteLine("\nRename a Folder/Text File:");
-                        //Files shown below
-                        break;
-                    case 4:
-                        Console.WriteLine("\nDelete a Folder/Text File:");
-                        //Files shown below
-                        break;
-                    default:
-                        Console.WriteLine("Invalid Option!");
-                        break;
+                    Console.WriteLine(i);
                 }
-            } while (opt < 1 || opt > 3);
+                Console.WriteLine("[6] Show Current Files");
+
+                do
+                {
+                    invalidOption = false;
+
+                    Console.Write("\nSelect an option: ");
+                    opt = Convert.ToInt16(Console.ReadLine());
+
+                    switch (opt)
+                    {
+                        case 1:
+                            Console.WriteLine("\nCreate a Folder/Text File:");
+                            Console.WriteLine("[1] Folder \n[2] Text File \n[3] Back");
+
+                            Console.Write("\nSelect an option: ");
+                            createOpt = Convert.ToInt16(Console.ReadLine());
+
+                            if (createOpt == 1)
+                            {
+                                Console.Write("\nEnter a name: ");
+                                string folderName = Console.ReadLine();
+                                list.Add(folderName);
+
+                                Console.WriteLine("Folder created succesfully!");
+                                clear = ClearConsole(clear);
+                            }
+                            else if (createOpt == 2)
+                            {
+                                Console.Write("\nEnter a name: ");
+                                string fileName = Console.ReadLine();
+                                list.Add(fileName + ".txt");
+
+                                Console.WriteLine("Text File created succesfully!");
+                                clear = ClearConsole(clear);
+                            }
+                            else if (createOpt == 3)
+                            {
+                                clear = ClearConsole(clear);
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine("\nOpen a Folder/Text File:");
+                            //Files shown below
+                            break;
+                        case 3:
+                            Console.WriteLine("\nRename a Folder/Text File:");
+                            //Files shown below
+                            break;
+                        case 4:
+                            Console.WriteLine("\nEdit a Text File:");
+                            //Files shown below
+                            break;
+                        case 5:
+                            Console.WriteLine("\nDelete a Folder/Text File:");
+                            //Files shown below
+                            break;
+                        case 6:
+                            Console.WriteLine("\nCurrent Files in YourFiles\\" + /*current folder*/ ":");
+                            //Files shown below
+                            foreach(string i in list)
+                            {
+                                Console.WriteLine("- " + i);
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Option!");
+                            invalidOption = true;
+                            break;
+                    }
+                } while (invalidOption != false);
+            } while (clear != false);
         }
 
         //methods
-        public void ClearConsole()
+        static bool ClearConsole(bool clearConsole)
         {
+            Console.WriteLine("\nClearing console...");
+            Thread.Sleep(3000);
+            Console.Clear();
 
+            return true;
+        }
+        static void ListOfFiles()
+        {
+            
         }
     }
 }
